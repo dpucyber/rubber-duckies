e52cc251e37d91e18e54b3f7610052298e3f7870
Galileo <46945263+641i130@users.noreply.github.com>
2021-11-12 19:53:45 -0600

    Update mouse.ino

--- src/mouse/mouse.ino
+++ src/mouse/mouse.ino
@@ -1,14 +1,14 @@
-#include "DigiKeyboard.h"
+#include <DigiMouse.h>
+
 void setup() {
-  DigiKeyboard.delay(2000); // Wait for 2 seconds
-  DigiKeyboard.sendKeyStroke(0); // Init keyboard
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); // Do WIN+R
-  DigiKeyboard.delay(600); // Wait .6 seconds
-  DigiKeyboard.print("https://youtu.be/xm3YgoEiEDc"); // Type url
-  DigiKeyboard.sendKeyStroke(KEY_ENTER); // Press enter!
-  DigiKeyboard.delay(10000); // wait 11.5 seconds
-  DigiKeyboard.print(" "); // Press space
-  DigiKeyboard.delay(600); // Wait .6 seconds
-  DigiKeyboard.print("f"); // Press space
+  long randNumber;
+  randomSeed(analogRead(0));
+  DigiMouse.begin();
 }
-void loop() {}
+
+void loop() {
+  DigiMouse.moveY(random(-100,100));
+  DigiMouse.update();
+  DigiMouse.moveX(random(-100,100)); 
+  DigiMouse.update();
+}
